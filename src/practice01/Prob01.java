8046437a6c317eb5ae765382012cec4174ea6f6b
Kim's fam <Kim's fam@DESKTOP-1I3L3M0>
2019-11-10 19:30:40 +0900

    while1

--- src/practice01/Prob01.java
+++ src/practice01/Prob01.java
@@ -9,5 +9,16 @@ public class Prob01 {
 		Scanner scanner = new Scanner( System.in );
 		
 		int number = scanner.nextInt();
+		int a = 1;
+	//아래 while 반복문을 이용하여 hello world를 입력받은 수 만큼 출력!
+			
+		
+		while( a <= number) {
+				a++;
+			System.out.println("hello world");
+			
+	
+		}
+		
 	}
 }
